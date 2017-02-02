b953978b919af3b52e5f5fca8a1ef67bf66cfd19
Andrius Rainys <andraitis@gmail.com>
2017-02-02 14:28:20 +0000

    configure for deployment

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 var express = require("express");
+var app = express();
+var path = require('path');
 
-var app = express()
+app.get('/', function(req, res) {//this is how you send/show html file?
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+app.get('/:time',function(req,res){
+    
+    function unixToNatural(unix) {
+        var date = new Date(unix * 1000);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var month = months[date.getMonth()];
+        var day = date.getDay();
+        var year = date.getFullYear();
+        var result = month + ' ' + day + ' ' + year;
+        return result;
+    }
+    
+    if(!isNaN(req.params.time)){//isNaN= is Not a Number // !isNaN= it's a number
+        var result = unixToNatural(req.params.time);
+        var data = {"unix": req.params.time,"natural":result};
+        res.json(data);
+    }else{
+        var natural = new Date(req.params.time);
+        if(!isNaN(natural)){
+            var unix = natural/2;
+            var data = {"unix": unix,"natural":req.params.time};
+            res.json(data);
+        }else{
+            res.json({"unix": null,"natural":null});
+        }
+    }
+});
 
 var port = 8080;
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!')
+  console.log('Example app listening on port ' + port + '!');
 })
